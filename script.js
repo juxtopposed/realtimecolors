36b10ebe043cc8398f016832df9ef0b08165c6d4
Juxtopposed <115173648+juxtopposed@users.noreply.github.com>
2023-04-02 12:55:04 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+const primaryColor = document.getElementById('prim');
+const secondaryColor = document.getElementById('sec');
+const primbuttnColor = document.getElementById('primbuttn');
+const secbuttnColor = document.getElementById('secbuttn');
+const accentColor = document.getElementById('accent');
+
+
+primaryColor.addEventListener('input', () => {
+  const primaryColorValue = primaryColor.value;
+  document.documentElement.style.setProperty('--primary', primaryColorValue);
+});
+
+secondaryColor.addEventListener('input', () => {
+    const secondaryColorValue = secondaryColor.value;
+    document.documentElement.style.setProperty('--secondary', secondaryColorValue);
+});
+
+primbuttnColor.addEventListener('input', () => {
+    const primbuttnColorValue = primbuttnColor.value;
+    document.documentElement.style.setProperty('--primbuttn', primbuttnColorValue);
+});
+
+secbuttnColor.addEventListener('input', () => {
+    const secbuttnColorValue = secbuttnColor.value;
+    document.documentElement.style.setProperty('--secbuttn', secbuttnColorValue);
+});
+
+accentColor.addEventListener('input', () => {
+    const accentColorValue = accentColor.value;
+    document.documentElement.style.setProperty('--accent', accentColorValue);
+});
+
+
+
+// Export button click event listener
+const exportButton = document.getElementById('export');
+exportButton.addEventListener('click', () => {
+  // Create array of color values
+  const colorValues = [
+    primaryColor.value,
+    secondaryColor.value,
+    primbuttnColor.value,
+    secbuttnColor.value,
+    accentColor.value
+  ];
+
+  // Create a canvas and draw color squares
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = colorValues.length * 50;
+  canvas.height = 50;
+  for (let i = 0; i < colorValues.length; i++) {
+    ctx.fillStyle = colorValues[i];
+    ctx.fillRect(i * 50, 0, 50, 50);
+  }
+
+  // Create a blob from the canvas and save it as a palette image
+  const paletteImage = canvas.toDataURL('image/png');
+  const paletteBlob = dataURItoBlob(paletteImage);
+  const paletteFile = new File([paletteBlob], 'palette.png', { type: 'image/png' });
+
+  // Create a text file with color values
+  const colorText = `Your selected colors:\n
+    Primary: ${primaryColor.value} (RGB: ${hexToRgb(primaryColor.value)})
+    Secondary: ${secondaryColor.value} (RGB: ${hexToRgb(secondaryColor.value)})
+    Primary Button: ${primbuttnColor.value} (RGB: ${hexToRgb(primbuttnColor.value)})
+    Secondary Button: ${secbuttnColor.value} (RGB: ${hexToRgb(secbuttnColor.value)})
+    Accent: ${accentColor.value} (RGB: ${hexToRgb(accentColor.value)})\n
+Thanks for using Real-time Colors!`;
+  const colorBlob = new Blob([colorText], { type: 'text/plain' });
+  const colorFile = new File([colorBlob], 'colors.txt', { type: 'text/plain' });
+
+  // Create a zip file and add the palette image and color text file
+  const zip = new JSZip();
+  zip.file(paletteFile.name, paletteFile);
+  zip.file(colorFile.name, colorFile);
+  zip.generateAsync({ type: 'blob' }).then(function (blob) {
+    saveAs(blob, 'colors.zip');
+  });
+});
+
+// Function to convert hex color to rgb color
+function hexToRgb(hex) {
+  const r = parseInt(hex.substring(1,3), 16);
+  const g = parseInt(hex.substring(3,5), 16);
+  const b = parseInt(hex.substring(5,7), 16);
+  return `${r}, ${g}, ${b}`;
+}
+
+// Function to convert data URI to blob
+function dataURItoBlob(dataURI) {
+  const byteString = atob(dataURI.split(',')[1]);
+  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: mimeString });
+}
+
